935cabbb858a83af7a34c609cc19c9127f3fa4b0
r-elopre <donvgdong052@gmail.com>
2024-03-16 21:53:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/permissionhandlingapp/PermissionDialog.kt
@@ -0,0 +1,102 @@
+package com.example.permissionhandlingapp
+
+import android.Manifest
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun PermissionDialog(
+    permissionTextProvider: PermissionTextProvider,
+    isPermanentlyDeclined: Boolean,
+    onDismiss: () -> Unit,
+    onOkClick: () -> Unit,
+    onGoToAppSettingsClick: () -> Unit,
+    modifier: Modifier = Modifier){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+                Column(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Divider()
+                    Text(text = if (isPermanentlyDeclined){
+                        "Grant Permission"
+                    }else{
+                        "Ok"
+                    },
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                if (isPermanentlyDeclined) {
+                                    onGoToAppSettingsClick()
+                                } else {
+                                    onOkClick()
+                                }
+                            }
+                            .padding(16.dp)
+                        )
+                }
+        },
+        title = {
+                Text(text = "Permission Required")
+        },
+        text = {
+               Text(
+                   text = permissionTextProvider.getDescription(
+                       isPermanentlyDeclined = isPermanentlyDeclined
+                   )
+               )
+        },
+        modifier = modifier
+    )
+}
+
+interface PermissionTextProvider{
+    fun getDescription(isPermanentlyDeclined: Boolean):String
+}
+
+class CameraPermissionTextProvider: PermissionTextProvider{
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+       return if (isPermanentlyDeclined){
+           "It seems you permanently declined camera permission." +
+                   "You can go to the app settings to grant it"
+       }else {
+           "This app needs access to your camera so that your friends can see you call."
+       }
+    }
+}
+
+class RecordAudioPermissionTextProvider: PermissionTextProvider{
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if (isPermanentlyDeclined){
+            "It seems you permanently declined microphone permission." +
+                    "You can go to the app settings to grant it"
+        }else {
+            "This app needs access to your microphone so that your friends can see hear you."
+        }
+    }
+}
+
+class PhoneCallPermissionTextProvider: PermissionTextProvider{
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if (isPermanentlyDeclined){
+            "It seems you permanently declined phone calling permission." +
+                    "You can go to the app settings to grant it"
+        }else {
+            "This app needs access to your phone calling so that your friends can talk to your friends."
+        }
+    }
+}
